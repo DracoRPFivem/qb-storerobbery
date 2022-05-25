db90cb130bf6b39e67b522488df8598cdf18ce47
jokernights <jokernights@gmail.com>
2022-05-25 09:57:54 -0400

    added ps exports

--- client/main.lua
+++ client/main.lua
@@ -439,7 +439,8 @@ end)
 RegisterNetEvent('qb-storerobbery:client:robberyCall', function(_, _, _, coords)
     if PlayerJob.name == "police" and onDuty then
         PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
-        TriggerServerEvent('police:server:policeAlert', 'Storerobbery in progress')
+        --TriggerServerEvent('police:server:policeAlert', 'Storerobbery in progress')
+        exports['ps-dispatch']:StoreRobbery(camId)
 
         local transG = 250
         local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
